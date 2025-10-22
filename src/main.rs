e14bcd4a8af8f6ccd310e9bec1d61d41b8f319ea
calc <maheshwarivinayak90@gmail.com>
2025-10-22 16:06:51 +0530

    added banner and clear screen

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use crate::parser::load_config;
 
 mod config;
 mod parser;
+mod print_banner;
 mod runner;
 
 #[tokio::main]
 async fn main() {
+    clearscreen::clear().expect("Failed to clear the screen");
     let config = load_config("fyrer.yml");
     let colors = vec![
         colored::Color::Green,
@@ -16,7 +18,7 @@ async fn main() {
         colored::Color::White,
     ];
     let mut handles = vec![];
-
+    print_banner::print_banner();
     for (i, service) in config.services.into_iter().enumerate() {
         let color = colors[i % colors.len()];
         let handle = tokio::spawn(runner::runner(service, color));
